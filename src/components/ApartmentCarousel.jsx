32d6ebd65b331a252a563c3da99ddbd33079d480
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2024-06-30 01:11:04 +0530

    space issue done

--- src/components/ApartmentCarousel.jsx
+++ src/components/ApartmentCarousel.jsx
@@ -291,7 +291,7 @@ const ApartmentCarousel = () => {
   };
 
   return (
-    <div className="max-w-6xl mx-auto p-4 pt-8 ">
+    <div className="max-w-6xl mx-auto p-4 pt-12">
       <div className="flex justify-between items-center mb-4">
         <h2 className="lg:text-2xl font-bold text-gray-800 sm:text-md">
           Popular Apartments in Bengaluru
